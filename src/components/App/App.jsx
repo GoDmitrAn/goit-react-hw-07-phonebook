63d616501a90303f6e6d6acb6be8c8fd2af42ab4
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-10-19 01:32:28 +0300

    save object in state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,39 @@
+import { customAlphabet } from 'nanoid';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactTitle } from 'components/ContactTitle/ContactTitle';
 import { SectionTitle } from 'components/SectionTitle/SectionTitle';
 import { Component } from 'react';
 import { FormBox, SectionBox } from './App.styled';
-
+const nanoid = customAlphabet('1234567890abcdef', 10);
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
+  };
+  handleInputChange = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const id = nanoid();
+    const array = [...this.state.contacts];
+    array.push({ name: this.state.name, id: id });
+    this.setState({ contacts: array });
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '' });
   };
   render() {
     return (
       <SectionBox>
         <SectionTitle title="Phonebook" />
         <FormBox>
-          <ContactForm />
+          <ContactForm
+            handleSubmit={this.handleSubmit}
+            handleInputChange={this.handleInputChange}
+            nameValue={this.state.name}
+          />
         </FormBox>
         React homework template
         <ContactTitle title="Contacts" />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,19 @@
-export const ContactForm = () => {
+export const ContactForm = ({ handleSubmit, handleInputChange, nameValue }) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Name
         <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          onChange={handleInputChange}
+          value={nameValue}
         />
       </label>
+      <button type="submit">Add contact</button>
     </form>
   );
 };
